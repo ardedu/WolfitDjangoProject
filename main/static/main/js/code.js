a1b948eb3cd5ce22f8fc30b979bc7674997afb10
ardedu <130398606+ardedu@users.noreply.github.com>
2023-05-29 20:50:47 +0200

    Add files via upload

--- main/static/main/js/code.js
+++ main/static/main/js/code.js
@@ -1,17 +1,30 @@
 
-let program = document.getElementById('prog');
-let blog = document.getElementById('blogs');
-
-if (program) {
-  program.addEventListener("click", () => {
-    document.getElementById("news").style.display = "none";
-    document.getElementById("programmes").style.display = "block";
+let open_aside = document.getElementById('open_aside');
+let aside = document.getElementById('aside');
+let close_aside = document.getElementById('close_aside')
+if (open_aside) {
+    open_aside.addEventListener("click", () => {
+    
+    aside.style.display = "block";
+    
+    aside.style.zIndex = "2";
+    aside.style.width = "300px";
+    aside.style.marginTop = "0";
+    
+    close_aside.style.display = 'block';
+    close_aside.style.zIndex = "3";
   });
 }
-if (blog) {
-blog.addEventListener("click", () => {
-    document.getElementById("news").style.display = "block";
-    document.getElementById("programmes").style.display = "none";
+
+if (close_aside) {
+  close_aside.addEventListener("click", () => {
+  
+  aside.style.display = "none";
+  
+ 
 });
 }
+
+
+
 
